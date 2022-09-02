5ffa1c8d275016a23720813f333a1b2e307f21c9
Abimbola Lolade <fawazudeenogunleye@gmail.com>
2022-09-02 20:23:21 +0100

    nySchool

---
+++ script.js
@@ -0,0 +1,8 @@
+let navLinks = document.getElementById('navbarResponsive');
+function showMenu() {
+    navLinks.style.display = 'block';
+}
+function hideMenu(){
+    navLinks.style.display = 'none';
+    
+}
